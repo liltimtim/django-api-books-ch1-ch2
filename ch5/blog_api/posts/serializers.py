59fcffdf9aa6e99a2446a96c62418431d87c4768
Timothy Dillman <timothydillman@Timothys-iMac.knology.net>
2019-02-20 19:54:45 -0500

    demonstrate ability to use foreign serializer in model

--- ch5/blog_api/posts/serializers.py
+++ ch5/blog_api/posts/serializers.py
@@ -3,13 +3,17 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'author', 'title', 'body', 'created_at')
-        model = Post
-
-
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = get_user_model()
         fields = ('id', 'username')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    profile = UserSerializer(source='authorId', read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'authorId', 'title', 'body', 'created_at', 'profile')
+
